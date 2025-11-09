81e83bef530f0df514553d5026e90607825af440
Simon Carlén <simon.carlen1@gmail.com>
2025-11-09 19:16:05 +0100

    vercel speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import SiteHeader from "./components/SiteHeader/SiteHeader";
 import SiteFooter from "./components/SiteFooter/SiteFooter";
 import { AuthProvider } from "@/context/AuthContext";
 import { SchoolProvider } from "@/context/SchoolContext";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
 const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"] });
@@ -19,6 +20,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="sv">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased font-sans bg-background text-foreground`}>
+        <SpeedInsights /> 
         <AuthProvider>
           <SchoolProvider>
             {/*<SiteHeader />*/}
