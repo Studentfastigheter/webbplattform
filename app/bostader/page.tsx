40402108998d82e611557bdc88c57db35731680a
Alvin Stallgård <alvinst@chalmers.se>
2025-11-23 15:22:11 +0100

    switchSelect_component

--- app/bostader/page.tsx
+++ app/bostader/page.tsx
@@ -1,10 +1,12 @@
+import { SwitchSelect } from "@/components/switchSelect";
+
 export default function Page() {
-    return (
-      <main className="p-6">
-        <h1 className="text-2xl font-semibold">[Sidan ska implementeras]</h1>
-        <p className="mt-2 text-sm text-gray-600">
-          Innehåll kommer senare.
-        </p>
-      </main>
-    );
-  }
+  return (
+    <main className="p-6">
+      <h1 className="text-2xl font-semibold">[Sidan ska implementeras]</h1>
+      <p className="mt-2 text-sm text-gray-600">Innehåll kommer senare.</p>
+
+      <SwitchSelect />
+    </main>
+  );
+}
