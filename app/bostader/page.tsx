53316b0f3ba05408b61ab1e74ccbe9647b18f744
Alvin Stallgård <alvinst@chalmers.se>
2025-11-24 06:46:31 +0100

    HousingInfoBox_component

--- app/bostader/page.tsx
+++ app/bostader/page.tsx
@@ -1,12 +1,31 @@
-import { SwitchSelect } from "@/components/switchSelect";
+import HousingInfoBox from "@/components/ui/housingInfoBox";
 
 export default function Page() {
   return (
-    <main className="p-6">
-      <h1 className="text-2xl font-semibold">[Sidan ska implementeras]</h1>
-      <p className="mt-2 text-sm text-gray-600">Innehåll kommer senare.</p>
+    <main className="p-12 flex justify-center">
+      <HousingInfoBox
+        rentText={3800}
+        moveInDate="2026-07-03"
+        lastApplyDate="2026-05-24"
 
-      <SwitchSelect />
+        width={340}
+        height={200}
+
+        className="
+          rounded-[45px]
+          bg-white
+          shadow-lg
+          px-10 py-8
+          gap-0      
+        "
+
+        rentClassName="text-black"
+        rentTextClassName="text-xl font-medium"
+
+        moveInClassName="text-sm"
+        lastApplyClassName="text-sm"
+      />
+
     </main>
   );
 }
