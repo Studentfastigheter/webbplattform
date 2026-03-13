20f367a2947ce3c5225ae3398f6f9ec6ea7c1344
Sumiya Sayeed <sumiyasayeed@gmail.com>
2026-03-13 17:18:41 +0100

    style: Add `overflow-hidden` and `relative` classes to the listing card image container.

--- components/Listings/ListingCard_Small.tsx
+++ components/Listings/ListingCard_Small.tsx
@@ -123,7 +123,7 @@ const ListingCardSmall: React.FC<ListingCardSmallProps> = (props) => {
     >
       {/* IMAGE */}
       <div
-        className="w-full bg-gray-100"
+        className="w-full bg-gray-100 overflow-hidden relative"
         style={{
           borderRadius: scaleValue(28),
           height: scaleValue(imageBaseHeight),
